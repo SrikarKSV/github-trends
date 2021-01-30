28408e758614fcbad0cb8b95e3f727522eb617d9
Srikar KSV <srikar1awesome@gmail.com>
2021-01-30 15:59:31 +0530

    📦 NEW: Added nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,26 @@
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Component } from 'react';
 
 export default class Nav extends Component {
   render() {
     return (
-      <div>
-        <h1>GitHub Trends</h1>
-      </div>
+      <header>
+        <div className='header-left'>
+          <h1>GitHub Trends</h1>
+          <nav>
+            <ul>
+              <li>User Search</li>
+              <li>Battle</li>
+            </ul>
+          </nav>
+        </div>
+        <div className='header-right'>
+          <button>
+            <FontAwesomeIcon size='2x' icon={faMoon} />
+          </button>
+        </div>
+      </header>
     );
   }
 }
