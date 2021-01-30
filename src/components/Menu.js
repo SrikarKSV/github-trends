abc586960eda9581375428623b79f78da6b8a06b
Srikar KSV <srikar1awesome@gmail.com>
2021-01-30 15:43:13 +0530

    📦 NEW: Setup the components for the homepage

---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Menu extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Find the trending repositories or developers on GitHub!</h2>
+      </div>
+    );
+  }
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <h1>GitHub Trends</h1>
+      </div>
+    );
+  }
+}
