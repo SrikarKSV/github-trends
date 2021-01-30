e50bd4ab16b76b4cda0e94d3ad4d0922150683d7
Srikar KSV <srikar1awesome@gmail.com>
2021-01-30 17:37:33 +0530

    👌 IMPROVE: Converted App component to class based

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import React, { Component } from 'react';
 import Menu from './components/Menu';
 import Nav from './components/Nav';
-// import Results from './components/Results';
+import Results from './components/Results';
 import './styles/main.scss';
 
-function App() {
-  return (
-    <div className='container'>
-      <Nav />
-      <Menu />
-      {/* <Results /> */}
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <Nav />
+        <main>
+          <Menu />
+          <Results />
+        </main>
+      </div>
+    );
+  }
 }
-
-export default App;
