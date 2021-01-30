ee3cca1249e2ebad3e6fe1cdcf56a1b5f107f0ee
Srikar KSV <srikar1awesome@gmail.com>
2021-01-30 16:31:51 +0530

    📦 NEW: Added menu and results component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react';
+import languages from '../data/githubLanguages';
 
 export default class Menu extends Component {
   render() {
     return (
-      <div>
-        <h2>Find the trending repositories or developers on GitHub!</h2>
-      </div>
+      <section className='menu-container'>
+        <h4>Find the trending repositories or developers on GitHub!</h4>
+        <div className='trend-select'>
+          <button>Repos</button>
+          <button>Users</button>
+        </div>
+        <div className='menu-options'>
+          <div className='menu-options__languages'>
+            <p>Languages :</p>
+            <ul>
+              <li>
+                <button>All</button>
+              </li>
+              <li>
+                <button>Python</button>
+              </li>
+              <li>
+                <button>JavaScript</button>
+              </li>
+              <li>
+                <button>Go</button>
+              </li>
+              <li>
+                <button>Css</button>
+              </li>
+              <li>
+                <select name='other-languages'>
+                  {Object.keys(languages).map((language) => (
+                    <option key={language} value={language}>
+                      {language}
+                    </option>
+                  ))}
+                </select>
+              </li>
+            </ul>
+          </div>
+          <div className='menu-options__date-range'>
+            <p>Data range :</p>
+            <ul>
+              <li>
+                <button>Daily</button>
+              </li>
+              <li>
+                <button>This week</button>
+              </li>
+              <li>
+                <button>This month</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
     );
   }
 }
---
+++ src/components/Results.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Results extends Component {
+  render() {
+    return (
+      <section>
+        <h4>Results</h4>
+      </section>
+    );
+  }
+}
