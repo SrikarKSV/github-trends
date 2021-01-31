78fc7d05ce83b1d0b8f769ea2b1d36e7ec6a03e9
Srikar KSV <srikar1awesome@gmail.com>
2021-01-31 09:40:59 +0530

    📦 NEW: Successfully fetched the data and stored in state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import Menu from './components/Menu';
 import Nav from './components/Nav';
@@ -6,21 +7,77 @@ import './styles/main.scss';
 
 export default class App extends Component {
   state = {
+    mode: 'repos',
     selectedLanguage: 'Any',
     selectedDate: 'daily',
+    repoData: {},
+    devData: {},
   };
 
   updateSelectedLanguage = (language) => {
     this.setState({
       selectedLanguage: language,
     });
+    this.fetchData();
   };
 
   updateSelectedDate = (date) => {
     this.setState({
       selectedDate: date,
     });
+    this.fetchData();
   };
+
+  updateMode = (mode) => {
+    this.setState({
+      mode,
+    });
+  };
+
+  setRepoLanguageState = (data) => {
+    this.setState(({ repoData, selectedDate, selectedLanguage }) => ({
+      repoData: {
+        ...repoData,
+        [selectedDate]: {
+          ...repoData[selectedDate],
+          [selectedLanguage]: data,
+        },
+      },
+    }));
+  };
+
+  fetchRepoData = async () => {
+    try {
+      const URL = `https://github.com/trending${
+        this.state.selectedLanguage === 'Any'
+          ? ''
+          : `/${this.state.selectedLanguage}`
+      }?since=${this.state.selectedDate}`;
+
+      const res = await axios.post('/api/getTrendingRepo', {
+        url: URL,
+      });
+      const data = await res.data;
+
+      this.setRepoLanguageState(data);
+    } catch (err) {
+      if (err.response.status) {
+        this.setRepoLanguageState('No trending repo/user found');
+      } else {
+        this.setRepoLanguageState('Error: Try again later');
+      }
+    }
+  };
+
+  fetchData = () => {
+    if (this.state.mode === 'repos') {
+      this.fetchRepoData();
+    }
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
 
   render() {
     return (
@@ -28,6 +85,7 @@ export default class App extends Component {
         <Nav />
         <main>
           <Menu
+            updateMode={this.updateMode}
             updateLanguage={this.updateSelectedLanguage}
             updateDate={this.updateSelectedDate}
           />
