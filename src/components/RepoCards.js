dd3ab26fac8cd3d103eb71b61a86cda6cc0e8a37
Srikar KSV <srikar1awesome@gmail.com>
2021-01-31 16:18:41 +0530

    📦 NEW: Added proptypes and made RepoCard component

---
+++ src/components/RepoCards.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCodeBranch, faStar } from '@fortawesome/free-solid-svg-icons';
+
+export default class RepoCards extends Component {
+  static propTypes = {
+    repos: PropTypes.oneOfType([PropTypes.array, PropTypes.string]),
+  };
+
+  render() {
+    const { repos } = this.props;
+    return (
+      <div className='repo-cards-container'>
+        {/* Checking if the response has a data or not */}
+        {Array.isArray(repos) ? (
+          repos.map((repo) => (
+            <article key={repo.authorImg}>
+              <img src={repo.authorImg} alt={repo.title} />
+              <div className='repo-cards-container__info'>
+                <h4>{repo.title}</h4>
+                <p>{repo.description}</p>
+                <p>Language : {repo.language}</p>
+                <p>
+                  <span>
+                    <FontAwesomeIcon icon={faStar} size='1x' />
+                    {repo.stars}
+                  </span>
+                  <span>
+                    <FontAwesomeIcon icon={faCodeBranch} size='1x' />
+                    {repo.forks}
+                  </span>
+                </p>
+                <div>
+                  <p>Contributors</p>
+                  <div className='contributors-images'>
+                    {repo.contributors.length
+                      ? repo.contributors.map((contributors) => (
+                          <a href={contributors.contributorsLink}>
+                            <img
+                              src={`${contributors.img}?s=60`}
+                              alt='Contributor avatar'
+                            />
+                          </a>
+                        ))
+                      : 'No Contributor'}
+                  </div>
+                </div>
+              </div>
+            </article>
+          ))
+        ) : (
+          <p>{repos}</p>
+        )}
+      </div>
+    );
+  }
+}
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import RepoCards from './RepoCards';
 
 export default class Results extends Component {
+  static propTypes = {
+    mode: PropTypes.string.isRequired,
+    repoData: PropTypes.object.isRequired,
+    devData: PropTypes.object.isRequired,
+    selectedLanguage: PropTypes.string.isRequired,
+    selectedDate: PropTypes.string.isRequired,
+  };
+
   render() {
+    const {
+      mode,
+      repoData,
+      devData,
+      selectedLanguage,
+      selectedDate,
+    } = this.props;
+
     return (
       <section>
         <h4>Results</h4>
+
+        {mode === 'repos' ? (
+          repoData?.[selectedDate]?.[selectedLanguage] ? (
+            <RepoCards repos={repoData[selectedDate][selectedLanguage]} />
+          ) : (
+            'Loading'
+          )
+        ) : devData?.[selectedDate]?.[selectedLanguage] ? (
+          JSON.stringify(devData[selectedDate][selectedLanguage])
+        ) : (
+          'Loading'
+        )}
       </section>
     );
   }
