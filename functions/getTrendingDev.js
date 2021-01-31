e33757fd20b00adf677467502d9df26290f60da0
Srikar KSV <srikar1awesome@gmail.com>
2021-01-31 10:32:01 +0530

    🐛 FIX: Reverted from seperate fetch script

--- functions/getTrendingDev.js
+++ functions/getTrendingDev.js
@@ -1,9 +1,19 @@
-const fetchData = require('./utils/fetchData');
+const axios = require('axios');
+const cheerio = require('cheerio');
 const formattedResponse = require('./utils/formattedResponse');
 
 exports.handler = async (event) => {
   try {
-    const { repos, $ } = await fetchData(event);
+    const { url } = JSON.parse(event.body);
+    const res = await axios.get(url, {
+      headers: {
+        Accept: 'text/html',
+      },
+    });
+    const text = await res.data;
+    const $ = cheerio.load(text);
+
+    const repos = $('.Box article.Box-row').toArray();
 
     const data = repos.map((repo) => {
       const active = $(repo);
