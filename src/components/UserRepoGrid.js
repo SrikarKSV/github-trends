157170d8b89282a95569b3434cdbfda68ded5ea2
Srikar KSV <srikar1awesome@gmail.com>
2021-02-03 20:55:35 +0530

    👌 IMPROVE: Informed user the issue with GitHub API for forked repos

--- src/components/UserRepoGrid.js
+++ src/components/UserRepoGrid.js
@@ -8,14 +8,28 @@ function UserRepoGrid({ userRepo }) {
       {userRepo.isForked && <span className='forked'>Forked</span>}
       <span className='no-stars'>{userRepo.stars}</span>
       <span className='no-forks'>{userRepo.forks}</span>
-      <a href={userRepo.repoLink}>
+      <a target='_blank' rel='noreferrer' href={userRepo.repoLink}>
         <h5>{userRepo.title}</h5>
       </a>
       <p>{userRepo.description}</p>
       <ul>
-        <li>Langauge : {userRepo.language}</li>
+        <li>
+          Langauge :
+          {userRepo.language
+            ? userRepo.language
+            : userRepo.isForked
+            ? 'GitHub API seems to not return langauge of forked repo'
+            : 'No language used'}
+        </li>
         <li>Created on : {formatDate(userRepo.createdAt)}</li>
-        {userRepo.homepage && <li>Homepage : {userRepo.homepage}</li>}
+        {userRepo.homepage && (
+          <li>
+            Homepage :{' '}
+            <a target='_blank' rel='noreferrer' href={userRepo.homepage}>
+              {userRepo.homepage}
+            </a>
+          </li>
+        )}
         <li>Open issues : {userRepo.openIssues}</li>
       </ul>
     </article>
