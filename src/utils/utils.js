09bd0e8cff461954ae20332b36375cf53759bace
Srikar KSV <srikar1awesome@gmail.com>
2021-02-03 20:56:35 +0530

    📦 NEW: Added functions to filter data for repos

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,5 +6,31 @@ export function formatDate(date) {
 }
 
 export function getUniqueLanguages(repos) {
-  return Array.from(new Set(repos.map((repo) => repo.language)));
+  return Array.from(
+    new Set(
+      repos.map((repo) => {
+        if (!repo.language) {
+          return 'No language used';
+        }
+        return repo.language;
+      })
+    )
+  );
+}
+
+export function filterRepoData(stars, language, repos) {
+  let newRepos = [...repos];
+  if (language) {
+    newRepos =
+      language === 'No language used'
+        ? (newRepos = newRepos.filter((repo) => !repo.language))
+        : newRepos.filter((repo) => repo.language === language);
+  }
+  if (stars) {
+    newRepos =
+      stars === 'asc'
+        ? newRepos.sort((repo1, repo2) => repo1.stars - repo2.stars)
+        : newRepos.sort((repo1, repo2) => repo2.stars - repo1.stars);
+  }
+  return newRepos;
 }
