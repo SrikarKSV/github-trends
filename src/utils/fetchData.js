a67cb5e699efa43741fb53785200045979096ad8
Srikar KSV <srikar1awesome@gmail.com>
2021-02-03 11:22:43 +0530

    📦 NEW: Added function to fetch user details

---
+++ src/utils/fetchData.js
@@ -0,0 +1,74 @@
+import axios from 'axios';
+
+function parseErrorMsg(message, username) {
+  if (message === 'Not Found') {
+    return `${username} doesn't exist`;
+  }
+  return message;
+}
+
+function formatRepoDetails(data) {
+  const repoDetails = data.map((repo) => {
+    return {
+      title: repo.name,
+      description: repo.description,
+      isForked: repo.fork,
+      repoLink: repo.html_url,
+      stars: repo.stargazers_count,
+      forks: repo.forks,
+      language: repo.language,
+      createdAt: repo.created_at,
+      homepage: repo.homepage,
+      openIssues: repo.open_issues,
+      size: repo.size,
+    };
+  });
+
+  return repoDetails;
+}
+
+function formatUserDetails(data) {
+  return {
+    profileLink: data.html_url,
+    avatarLink: data.avatar_url,
+    fullName: data.name,
+    username: data.login,
+    bio: data.bio,
+    company: data.company,
+    website: data.blog,
+    location: data.location,
+    twitterUsername: data.twitter_username,
+    followers: data.followers,
+    following: data.following,
+    noOfRepos: data.public_repos,
+  };
+}
+
+function fetchUserDetails(username, url, mode) {
+  return axios
+    .get(url)
+    .then(({ data }) => {
+      if (mode === 'user') {
+        return formatUserDetails(data);
+      } else {
+        return formatRepoDetails(data);
+      }
+    })
+    .catch((err) => {
+      if (err.message) {
+        return parseErrorMsg(err.message, username);
+      }
+      return err;
+    });
+}
+
+export function getUserDetails(username, page = 1) {
+  const userProfileURL = `https://api.github.com/users/${username}`;
+  const userRepoURL = `https://api.github.com/users/${username}/repos?per_page=100&type=owner&sort=updated&page=${page}`;
+  return Promise.all([
+    fetchUserDetails(username, userProfileURL, 'user'),
+    fetchUserDetails(username, userRepoURL, 'repo'),
+  ]).then(([profile, repo]) => {
+    return { profile, repo };
+  });
+}
