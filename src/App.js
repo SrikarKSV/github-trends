ebac1d89b09c0c088ceb1c56f23aa6b40f9ce3d9
Srikar KSV <srikar1awesome@gmail.com>
2021-02-04 21:24:18 +0530

    📦 NEW: Added state for the context

--- src/App.js
+++ src/App.js
@@ -4,22 +4,34 @@ import Home from './components/Home';
 import UserSearch from './components/UserSearch';
 import './styles/main.scss';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from './contexts/theme';
 
 export default class App extends Component {
+  state = {
+    theme: 'dark',
+    toggleTheme: () => {
+      this.setState(({ theme }) => ({
+        theme: theme === 'dark' ? 'light' : 'dark',
+      }));
+    },
+  };
+
   render() {
     return (
       <Router>
-        <div className='bg-dark'>
-          <div className='container'>
-            <Nav />
-            <main>
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route path='/user-search' component={UserSearch} />
-              </Switch>
-            </main>
+        <ThemeProvider value={this.state}>
+          <div className={`bg-${this.state.theme}`}>
+            <div className='container'>
+              <Nav />
+              <main>
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route path='/user-search' component={UserSearch} />
+                </Switch>
+              </main>
+            </div>
           </div>
-        </div>
+        </ThemeProvider>
       </Router>
     );
   }
